c763428eb0b4f443be1a41b034f7e15395b25b01
rcrespop <robertocre@gmail.com>
2015-08-16 14:04:06 +0200

    commit para probar jenkins

--- web/src/main/java/net/rc/presentacion/SaludoServlet.java
+++ web/src/main/java/net/rc/presentacion/SaludoServlet.java
@@ -1,13 +1,10 @@
 package net.rc.presentacion;
-
 import java.io.IOException;
 import java.io.PrintWriter;
-
-import javax.servlet.GenericServlet;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,15 +14,19 @@ import javax.servlet.http.HttpServletResponse;
 *
 */
 
-public class SaludoServlet extends HttpServlet
+public class SaludoServlet extends HttpServlet implements Serializable
 {     
-	PrintWriter out = null;
+	
+	private static final Logger LOGGER = Logger.getLogger( SaludoServlet.class.getName() );
 
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
      
 		 processRequest(req, resp);
 	}
+	
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
@@ -35,16 +36,20 @@ public class SaludoServlet extends HttpServlet
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-			System.out.println("Procesando petición.. ");
+			//Equivalente al system.out.println
+			LOGGER.log(Level.FINE, "Procesando petición.. "); 
+			
 			String txtNombre = request.getParameter("txtNombre");
 
 
-			  out = response.getWriter();
-			  response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			response.setContentType("text/html");
 			  
 			  out.println("<html>");
 			  out.println("<body>");
 			  out.println("<br>");
+			  out.println("Hello World");
+			  out.println("<br>"); 
 			  out.println("Hello " + txtNombre);			  
 			  out.println("</body>");
 			  out.println("</html>");
